00d32a88d456a6274a822c1b0949918356c3e8c3
Abhishek7632 <99984657+Abhishek7632@users.noreply.github.com>
2023-07-11 20:46:15 +0530

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 143, 51, 51);
+  static Color secondaryColor = Color.fromARGB(255, 216, 164, 6);
+  static Color accentColor = Color(0xFF4169e8);
+}
